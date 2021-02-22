bc2e20f83b9554e11e6b1bb9874ecfcc3de1a9be
wallmar <markus-wallner@outlook.com>
2021-02-22 11:00:37 +0100

    Add chrome extension to auto-save har-files

---
+++ chrome-extension/har-saver.js
@@ -0,0 +1,20 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.status === "complete") {
+        setTimeout(() => {
+            chrome.devtools.network.getHAR(harLog => {
+                let harObj = {}
+                harObj.log = harLog
+
+                let blob = new Blob([JSON.stringify(harObj)])
+                let url = URL.createObjectURL(blob);
+                const date = new Date()
+                const formattedDate = `${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`
+
+                chrome.downloads.download({
+                    filename: `har/${new URL(tab.url).host}-${formattedDate}.json`,
+                    url
+                });
+            })
+        }, 1000);
+    }
+})
