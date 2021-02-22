b9d35f01383250b3b47ee5b175a04d603443d6d5
wallmar <markus-wallner@outlook.com>
2021-02-22 11:00:51 +0100

    Update filename

--- chrome-extension/har-saver.js
+++ chrome-extension/har-saver.js
@@ -11,7 +11,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                 const formattedDate = `${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`
 
                 chrome.downloads.download({
-                    filename: `har/${new URL(tab.url).host}-${formattedDate}.json`,
+                    filename: `har/${new URL(tab.url).host}-${formattedDate}`,
                     url
                 });
             })
