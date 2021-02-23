c37ae762bc3792466513ccef2145b5edc9b0872b
wallmar <markus-wallner@outlook.com>
2021-02-23 22:58:00 +0100

    Fix bug where duplicates of one sample lead to different filenames

--- chrome-extension/har-saver.js
+++ chrome-extension/har-saver.js
@@ -9,14 +9,12 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
                 let blob = new Blob([JSON.stringify(harObj)])
                 let url = URL.createObjectURL(blob);
-                const date = new Date()
-                const formattedDate = `${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`
                 const hostName = new URL(tab.url).host
 
                 if (hostName.startsWith('sample-')) {
                     // eslint-disable-next-line no-undef
                     chrome.downloads.download({
-                        filename: `har/${hostName}-${formattedDate}`,
+                        filename: `har/${hostName}`,
                         url
                     });
                 }
