c7d73611112b3e29dc30c1158270fd5888e06101
wallmar <markus-wallner@outlook.com>
2021-04-25 16:36:38 +0200

    Update samplesCount

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.config = {
     downloadsPath: '/home/mwallner/Downloads',
     networkInterface: 'enp30s0',
-    samplesCount: 5,
+    samplesCount: 18,
     samplesDomain: 'mmtsample.hopto.org',
     samplesStartWith: 50,
     serverNetworkInterface: 'enxa0cec83a9ce6',
